408a7c0429fcc20c74b5f5803da9d3b39c729654
anaherrera <anali.mogollon@gmail.com>
2015-09-17 08:04:55 -0500

    Create arreglosprueba.cpp

---
+++ arreglosprueba.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+   int v[]={2,4,6,8,1,0};
+   int n,i;
+   cout<<" ingrese numero:";
+   cin>>n;
+   for(i=1;i<=10;i++)
+   if (i==v[i])
+   {
+   cout<<"  si lo contiene";
+	return 0;
+   }
+cout<<"  no lo contiene";
+return 0;
+}
