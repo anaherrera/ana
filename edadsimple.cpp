dcf0a43481751f671d34eb203db9c2670e9e822b
anaherrera <anali.mogollon@gmail.com>
2015-09-17 07:45:59 -0500

    Create edadsimple.cpp

---
+++ edadsimple.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a;
+	cout<<"ingrese su edad"; //le pides datos  
+	cin>>a;// lo almacena en a
+   
+if (a>=20){
+ cout<<"usted es mayor";
+
+ }
+else{
+ cout<<"usted es menor";
+}
+return 0;
+}
