518248e1eb104e669f7663555ff92b450bd53c96
anaherrera <anali.mogollon@gmail.com>
2015-09-17 07:47:20 -0500

    Create do-while.cpp

---
+++ do-while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	int numero;
+	do
+	{
+		cout <<  "Ingrese un numero ";
+		cin >> numero;
+	}
+	while(numero <= 100);
+	system("PAUSE");
+	return 0;
+}
