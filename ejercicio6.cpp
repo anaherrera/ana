0340473ef6aa66cf50b63d155975740b3ef612ea
anaherrera <anali.mogollon@gmail.com>
2015-09-28 15:18:37 -0500

    Create ejercicio6.cpp

---
+++ ejercicio6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int ana[8],i;
+	cout << "ingrese los numero :" << endl;
+
+        for(int i = 0; i < 8; i++)
+            cin >> ana[i];
+
+	cout << "los valores en orden inverso son:";
+        for(int i = TAM; i>=0; i--)
+            cout << " " << ana[i];
+            cout << endl;
+}
