ae66d8536b0895b5c76e0ab62a2caf8ce385fe8e
anaherrera <anali.mogollon@gmail.com>
2015-09-28 17:30:41 -0500

    Create ejercicio8.cpp

---
+++ ejercicio8.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n,a,b;
+n=1;
+a=0;
+
+for(int i=1;i<5;i++){
+        //cout<<i<<"\n";
+        for(int j=1;j<8;j++){
+            if(j>=i){
+             
+                if(j%i==0){
+               // cout<<"..."<<i<<"/"<<j<<i/j<<"++++";
+		
+                }
+            else
+
+                //cout<<"no hay dicho numero probamos otro numero";
+                break;
+
+            }
+}
+        }
+}
