530f745bd404ec4d3069fc999e39c5471a531052
anaherrera <anali.mogollon@gmail.com>
2015-09-28 17:47:43 -0500

    Update ejercicio6.cpp

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
 
-	int ana[8],i;
-	cout << "ingrese los numero :" << endl;
+int main(){
+    int m3,m5;
+    m3=0;
+    m5=0;
+    for(int i = 1; i < 1000; i++){
+    //cout<<i;
+        if(i%3==0)
+            m3=m3+i;
+            //cout<<i;
+        if(i%5==0)
+            m5=m5+i;
+           //cout<<i;
+    }
+    cout<<m3+m5;
 
-        for(int i = 0; i < 8; i++)
-            cin >> ana[i];
-
-	cout << "los valores en orden inverso son:";
-        for(int i = TAM; i>=0; i--)
-            cout << " " << ana[i];
-            cout << endl;
 }
+
