6e6ee3fbcdf74187cbb5d499bd9e93deea8f4a97
anaherrera <anali.mogollon@gmail.com>
2015-10-01 08:40:41 -0500

    Create ejercicio3.cpp

---
+++ ejercicio3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a,n;
+bool b;
+b=false;
+a=0;
+for(int  long i=2520;i<2526;i++)
+//n=2520;
+
+        for(int j=1;j<21;j++)
+        {
+
+            if(n%j==0)
+            {
+
+                a=a+1;
+                //cout<<a;
+            }
+          
+ if(a==20)
+    {
+    cout<<n<<"es divisible con cada numero del 1-20";
+    break;
+
+    }
+    
+        }
+
+
+    }
+
+
