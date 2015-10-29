bc069c8baef48480b1087988100497473cba8a02
anaherrera <anali.mogollon@gmail.com>
2015-10-29 08:35:13 -0500

    Create punto.cpp

---
+++ punto.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "apunto.h"
+
+using namespace std;
+void punto::punto(double x, double y)) {
+   :x(x), y(y) {}
+   //x = x1;
+   //y = y1;
+
+}
+void punto::printpun(){
+	cout<<"la coor en x del primer punto es:"<<x1<<endl;
+	cout<<"la coor en y del primer punto es:"<<y1<<endl;
+}
