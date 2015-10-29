32d9dc9b985b05e60a612d073fdbec6fce428d50
anaherrera <anali.mogollon@gmail.com>
2015-10-29 08:36:37 -0500

    Create vector.h

---
+++ vector.h
@@ -0,0 +1,14 @@
+#include "apunto.h"
+
+class vector{
+    public:
+    // Constructor
+      vector(double start, double end);
+      double distancia();
+      void print();
+
+	private:
+		punto start,end;
+		//double distancia();
+		//void print();
+};
