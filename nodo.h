6082a445030668b7f339e87500024d1d5cca5431
anaherrera <anali.mogollon@gmail.com>
2015-11-16 17:05:28 -0500

    Create nodo.h

---
+++ nodo.h
@@ -0,0 +1,16 @@
+#ifndef NODO_H
+#define NODO_H
+
+
+class nodo
+{
+    public:
+        nodo(int);
+        //virtual ~nodo();
+    //protected:
+    private:
+        int dato;
+        nodo *sig;
+};
+
+#endif // NODO_H
