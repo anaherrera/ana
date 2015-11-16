a84aea0abfa8018a033e2fafc8d0291e1a79d0d0
anaherrera <anali.mogollon@gmail.com>
2015-11-16 17:06:31 -0500

    Create mainlista.cpp

---
+++ mainlista.cpp
@@ -0,0 +1,18 @@
+#include <iostream>//importar un archivo
+using namespace std;//buscar un archivo
+# include"lista.h"
+int main()
+{
+    lista a;
+    a.agregar(3);
+    a.agregar(4);
+    a.agregar(333);
+    a.agregar(56);
+    a.agregar(78);
+    a.agregar(111);
+
+    a.imprimir ();
+    //a.borrar(4);
+    return 0;
+
+}
