df1c95909e9fe8cbca5d8249d1ffe3448ba6f758
anaherrera <anali.mogollon@gmail.com>
2015-11-16 17:03:23 -0500

    Create lista.cpp

---
+++ lista.cpp
@@ -0,0 +1,37 @@
+#include "lista.h"
+#include <iostream>
+using namespace std;
+
+lista::lista()
+{
+    inicio=fin=0
+}
+bool lista::agregar(int dato)
+{
+    if(inicio)
+    {
+        inicio=new nodo(dato);
+        fin=inicio;
+    }
+    else
+    {
+        fin->sig=new nodo(dato);
+        fin=fin->sig;
+    }
+
+}
+//return 1;
+{
+bool lista::imprimir()
+    nodo*tmp=inicio;
+    while(tmp)
+    {
+        cout<<tmp->dato<<endl;
+        tmp=tmp->sig;
+    }
+
+   //this lista=0 //ctor
+}
+void lista::Borrar(int dato) {
+
+   }
