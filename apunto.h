f0187f75ba2244114e8cdee92fc58741f9a21448
anaherrera <anali.mogollon@gmail.com>
2015-11-16 17:46:31 -0500

    Create apunto.h

---
+++ apunto.h
@@ -0,0 +1,10 @@
+ template <typename T>
+class punto {
+    public:
+    // Constructor
+      punto(double T x1, double T y1);
+      void printpun(double x1 , double y1);
+	private:
+	 T double x,y;
+		//void printpun();
+};
