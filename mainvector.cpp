4f637233e4d001569f5f8fa39964db2cd15d4e2e
anaherrera <anali.mogollon@gmail.com>
2015-11-16 17:47:52 -0500

    Create mainvector.cpp

---
+++ mainvector.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "apunto.h"
+
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+
+    punto p(3.3,2.2);
+    double a,b;
+     punto s(3.8,1.2);
+    double a,b;
+
+
+    p.printpun(a,b);
+        return 0;
+}
