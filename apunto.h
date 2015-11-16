30f5e33fa81c3ad7f4d7b834eec4ac296497dbad
anaherrera <anali.mogollon@gmail.com>
2015-11-16 17:53:13 -0500

    Update apunto.h

--- apunto.h
+++ apunto.h
@@ -3,7 +3,7 @@ class punto {
     public:
     // Constructor
       punto(double T x1, double T y1);
-      void printpun(double x1 , double y1);
+      void printpun(double T x1 , double T y1);
 	private:
 	 T double x,y;
 		//void printpun();
